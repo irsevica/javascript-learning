a0dcb539552bc1bd35af8082a878d4509d97236e
Anita Irsevic <anitai@datacom.co.nz>
2018-05-27 22:54:57 +1200

    add function test  script

---
+++ sandwhich.js
@@ -0,0 +1,7 @@
+function makeSandwichWith(filling) {
+  var sicksandwich = ['bread', 'butter', 'pickles', 'jam']
+  sicksandwich.push(filling)
+  return console.log("Here you go, your", filling, "sandwich is ready. It is made out of", sicksandwich)
+}
+
+makeSandwichWith('mushroom')
